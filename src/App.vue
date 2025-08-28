45985a9825dfe7bf0ddfad7b479b4ec783b05880
Nicholas Us <nicholas.us.03@gmail.com>
2025-08-28 19:03:30 +0300

    config postcss

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,5 @@
-<script setup></script>
-
 <template>
-  <h1>You did it!</h1>
-  <p>
-    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
-    documentation
-  </p>
+  <div class="flex min-h-screen items-center justify-center bg-gray-100">
+    <h1 class="text-3xl font-bold text-blue-600">Hello Tailwind + Vue 🚀</h1>
+  </div>
 </template>
-
-<style scoped></style>
---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+
+import defaultTheme from 'tailwindcss/defaultTheme';
+import forms from '@tailwindcss/forms';
+import typography from '@tailwindcss/typography';
+
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{vue,js,ts,jsx,tsx}",
+  ],
+   theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Figtree', ...defaultTheme.fontFamily.sans],
+            },
+        },
+    },
+
+    plugins: [forms, typography],
+}
